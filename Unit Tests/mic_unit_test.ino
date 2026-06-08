7f7cac0223d6d291ec8e63b8e6cb68661df57c79
matan biton <85506276+MatanBiton@users.noreply.github.com>
2026-06-08 17:45:18 +0300

    Change I2S microphone channel and pin assignments

--- Unit Tests/mic_unit_test.ino
+++ Unit Tests/mic_unit_test.ino
@@ -4,18 +4,18 @@
 #define SAMPLE_BUFFER_SIZE 512
 #define SAMPLE_RATE 8000
 // most microphones will probably default to left channel but you may need to tie the L/R pin low
-#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
+#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_RIGHT
 // either wire your microphone to the same pins or change these to match your wiring
-#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_26
-#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_22
-#define I2S_MIC_SERIAL_DATA GPIO_NUM_21
+#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_32
+#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_25
+#define I2S_MIC_SERIAL_DATA GPIO_NUM_33
 
 // don't mess around with this
 i2s_config_t i2s_config = {
     .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
     .sample_rate = SAMPLE_RATE,
     .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+    .channel_format = I2S_MIC_CHANNEL,
     .communication_format = I2S_COMM_FORMAT_I2S,
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
     .dma_buf_count = 4,
